e347c4e9bcd71636df81996e04e8ffb734d5ac7c
blueGLS <blueglasses1054@yahoo.co.jp>
2015-05-15 19:43:52 +0900

    change 4(by Nagata)

--- tomato/tomato.cpp
+++ tomato/tomato.cpp
@@ -2,7 +2,7 @@
 #include "opencv_lib.hpp"	
 #include <math.h>                // OpenCVヘッダ
 #include <time.h>
-// change 3
+// change 4(by nagata)
 #define PI (atan(1.0)*4)
 
 int Hmin = 5, Hmax = 35;										// 色相初期値
